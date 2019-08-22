44d5597ed5f96541f6ca9e53fda9af766914d559
kifonggg <53514960+kifonggg@users.noreply.github.com>
2019-08-22 14:04:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,15 +49,42 @@ def webhook():
                     sender_id = messaging_event['sender']['id']
                     message_text = messaging_event['message']['text']
 
-                    send_result = reply_message(recipient_id = sender_id, content = 'Hello World!as;dmjc!!', message_type = 'message')
+                    button_type = ['postback', 'postback', 'postback']
+                    button_title = ['Metrics Value', 'Metrics Definition', 'Subscription Setting']
+                    button_payload = ['Metrics Value', 'Metrics Definition', 'Subscription Setting']
+                    button_text = 'Hi, what would you like to get from me?'
+                    reply_message = {
+                            'message_format': 'button',
+                            'type': button_type,
+                            'title': button_title,
+                            'payload': button_payload,
+                            'text': button_text
+                    }
+
+                    send_result = reply_message(recipient_id = sender_id, content = reply_message, message_type = 'message')
                     log(send_result)
 
     return "Done"
 
             
 def reply_message(recipient_id, content, message_type):
-    log('sending message to {recipient}: {text}'.format(recipient=recipient_id, text=content))
-    r = bot.send_text_message(recipient_id = recipient_id, message = content, message_type = message_type)
+    #content['message_format'] = ['text', 'button', 'quick_reply', 'attachemnt']
+
+    if content['message_format'] == 'text':
+        log('sending message to {recipient}: {text}'.format(recipient=recipient_id, text=content))
+        r = bot.send_text_message(recipient_id = recipient_id, message = content['text'], message_type = message_type)
+
+    elif content['message_format'] == 'button':
+        log('sending button message to {recipient}: {text}'.format(recipient=recipient_id, text=content))
+        r = bot.send_button_message(recipient_id = recipient_id, message = content['text'], button_type = content['type'], button_titles = content['title'], payload = content['payload'], message_type = message_type)
+
+    elif content['message_format'] == 'quick_reply':
+        log('sending quick reply message to {recipient}: {text}'.format(recipient=recipient_id, text=content))
+        r = bot.send_quick_reply_message(recipient_id = recipient_id, message = content['text'], content_type = content['type'], reply_titles = content['title'], payload = content['payload'], message_type = message_type)
+
+    elif content['message_format'] == 'attachemnt':
+        log('sending attachemnt to {recipient}: {text}'.format(recipient=recipient_id, text=content))
+        r = bot.send_text_message(recipient_id = recipient_id, message = content['text'], message_type = message_type)
 
     return r
     
