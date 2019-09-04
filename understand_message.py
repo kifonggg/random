f46c750a973400c7a54d03f176f19576dda01edd
kifonggg <53514960+kifonggg@users.noreply.github.com>
2019-09-04 15:08:03 +0800

    Update understand_message.py

--- understand_message.py
+++ understand_message.py
@@ -1,6 +1,6 @@
 
 from fuzzywuzzy import fuzz, process
-#from metrics_ask import Metrics_Doctor
+from metrics_ask import Metrics_Doctor
 #from definition_ask import Definition_Doctor
 #from subscription_ask import Subscription_Doctor
 
@@ -9,11 +9,12 @@ class Professor_X(object):
     def __init__(self, message_text):
         self.text = message_text
         self.initial_word = ['hi', 'hello', 'hey', 'help', 'get started']
+        self.end_word = ['thanks', 'bye', 'thank you', 'good', 'ok']
         
     
     def memory_brain(self):
-        
-        # check initial memory
+
+    	# check initial memory
         try:
             file_r = open('memory_file.txt', 'r')
             my_memory = file_r.read()
@@ -22,7 +23,7 @@ class Professor_X(object):
             file_.close()
             file_r = open('memory_file.txt', 'r')
             my_memory = file_r.read()
-             
+        
         if process.extractOne(self.text, self.initial_word, scorer = fuzz.ratio)[1] >= 80:
             file_w = open('memory_file.txt', 'w')
             file_w.write('')
@@ -45,29 +46,28 @@ class Professor_X(object):
 
     def get_reply(self):
         memory = self.memory_brain()
-        print('the current memory:' + memory)
+        print('Professor_X current memory:' + memory)
     
         #clean_text = process.extractOne(self.text, initial_word, scorer = fuzz.ratio)[0]
 
         if memory == 'Metrics Value':
-            #reply_message = Metrics_Doctor(self.text).get_reply_message()
-            reply_message = {
-                'message_format': 'text',
-                'text': 'Hey I know you are asking me about Definition'
-            }
+            reply_message = Metrics_Doctor(self.text).get_reply_message()
+            
 
         elif memory == 'Metrics Definition':
             #reply_message = Definition_Doctor(self.text).get_reply_message()
             reply_message = {
                     'message_format': 'text',
-                    'text': 'Hey I know you are asking me about Definition'
+                    'text': 'Hey I know you are asking me about Definition. Unfortunely, this service it not available yet. Please stay tuned for it. =)\n'+
+                            'Say "Hi" to me for restarting the process.'
             }
 
         elif memory == 'Subscription Setting':
             #reply_message = Subscription_Doctor(self.text).get_reply_message()
             reply_message = {
                     'message_format': 'text',
-                    'text': 'Hey I know you are asking me about Subscription'
+                    'text': 'Hey I know you are asking me about Subscription. Unfortunely, this service it not available yet. Please stay tuned for it. =)\n'+
+                            'Say "Hi" to me for restarting the process.'
             }
 
 
@@ -76,7 +76,7 @@ class Professor_X(object):
             button_type = ['postback', 'postback', 'postback']
             button_title = ['Metrics Value', 'Metrics Definition', 'Subscription Setting']
             button_payload = ['Metrics Value', 'Metrics Definition', 'Subscription Setting']
-            button_text = 'Hi, what would you like to get from me?'
+            button_text = 'Hi, how would you like to help you?'
             reply_message = {
                     'message_format': 'button',
                     'type': button_type,
